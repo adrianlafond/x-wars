cd14f5e020f0a04837b0c1561e68bfa194044a56
Adrian Lafond <mail@adrianlafond.com>
2018-02-08 20:52:32 -0500

    add player storage (pockets) tests

--- src/player.js
+++ src/player.js
@@ -1,12 +1,16 @@
 import isPlainObject from 'lodash.isplainobject'
+import cloneDeep from 'lodash.clonedeep'
+
+const DATA = new WeakMap()
 
 /**
  *
  */
 export default class Player {
-  constructor(data) {
-    if (isPlainObject(data)) {
-      this.data = data
+  constructor(initialData) {
+    if (isPlainObject(initialData)) {
+      const data = cloneDeep(initialData)
+      DATA.set(this, data)
 
       // health
       data.health.min = Math.min(data.health.max, data.health.min)
@@ -20,26 +24,45 @@ export default class Player {
       data.wealth.min = Math.min(data.wealth.max, data.wealth.min)
       this.wealth = data.wealth.value
 
+      // storage
+      data.storage.min = (data.storage.min === '*') ? Number.MIN_VALUE :
+        data.storage.min
+      data.storage.max = (data.storage.max === '*') ? Number.MAX_VALUE :
+        data.storage.max
+      data.storage.min = Math.min(data.storage.max, data.storage.min)
+      this.storage = data.storage.value
     } else {
       throw new Error('Player data must be type "object".')
     }
   }
 
   get health() {
-    return this.data.health.value
+    return DATA.get(this).health.value
   }
 
   set health(value) {
-    this.data.health.value = Math.max(this.data.health.min,
-      Math.min(this.data.health.max, value))
+    const data = DATA.get(this)
+    data.health.value = Math.max(data.health.min,
+      Math.min(data.health.max, value))
   }
 
   get wealth() {
-    return this.data.wealth.value
+    return DATA.get(this).wealth.value
   }
 
   set wealth(value) {
-    this.data.wealth.value = Math.max(this.data.wealth.min,
-      Math.min(this.data.wealth.max, value))
+    const data = DATA.get(this)
+    data.wealth.value = Math.max(data.wealth.min,
+      Math.min(data.wealth.max, value))
+  }
+
+  get storage() {
+    return DATA.get(this).storage.value
+  }
+
+  set storage(value) {
+    const data = DATA.get(this)
+    data.storage.value = Math.max(data.storage.min,
+      Math.min(data.storage.max, value))
   }
 }
--- test/player.spec.js
+++ test/player.spec.js
@@ -1,28 +1,27 @@
 import Player from '../src/player'
 import defaults from '../src/defaults'
 
-describe('Player defaults', () => {
+describe('Player', () => {
 
   let player
   let health = defaults.player.health.value
   let wealth = defaults.player.wealth.value
+  let storage = defaults.player.storage.value
 
   beforeEach(() => {
     player = new Player(defaults.player)
   })
 
-  it('has data', () => {
-    expect(player.data).toEqual(defaults.player)
+  it('data is private', () => {
+    expect(player.data).toEqual(undefined)
   })
 
   // Health
   it('starts with perfect health', () => {
     expect(player.health).toBe(health)
   })
-  it('increments health down', () => {
+  it('increments health', () => {
     expect(--player.health).toBe(health - 1)
-  })
-  it('adds health up', () => {
     expect(++player.health).toBe(health)
   })
   it('prevents health from descending below minimum', () => {
@@ -38,10 +37,8 @@ describe('Player defaults', () => {
   it('starts with a wealth value', () => {
     expect(player.wealth).toBe(wealth)
   })
-  it('increments wealth down', () => {
+  it('increments wealth', () => {
     expect(--player.wealth).toBe(wealth - 1)
-  })
-  it('adds wealth up', () => {
     expect(++player.wealth).toBe(wealth)
   })
   it('prevents wealth from descending below minimum', () => {
@@ -53,6 +50,33 @@ describe('Player defaults', () => {
     expect(player.wealth).toBe(Number.MAX_VALUE)
     player.wealth = 999999999
     expect(player.wealth).toBe(999999999)
+  })
+
+  // Storage
+  it('starts with a storage value', () => {
+    expect(player.storage).toBe(storage)
+  })
+  it('increments wealth', () => {
+    expect(--player.storage).toBe(storage - 1)
+    expect(++player.storage).toBe(storage)
+  })
+  it('prevents storage from descending below minimum', () => {
+    player.storage = defaults.player.storage.min - 100
+    expect(player.storage).toBe(0)
+  })
+  it('allows storage to reach Number.MAX_VALUE', () => {
+    player.storage = Number.MAX_VALUE + 1
+    expect(player.storage).toBe(Number.MAX_VALUE)
+    player.storage = 999999999
+    expect(player.storage).toBe(999999999)
+  })
+
+  // Weapons
+  // it('weapons length must >= 0', () => {
+  //   expect(player.weapons.length).toBeGreaterThanOrEqual(0)
+  // })
+  it('allows weapons to be added', () => {
+    // expect(player.weapons.)
   })
 })
 
